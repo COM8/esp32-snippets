b34f44bbb10e549eacd0a65db7e7bcec8f48a450
Fabian Sauter <sauter.fabian@mailbox.org>
2020-08-08 14:28:15 +0200

    Ported nkolban/esp32-snippets/pull/994

--- cpp_utils/BLEAddress.h
+++ cpp_utils/BLEAddress.h
@@ -9,9 +9,9 @@
 #define COMPONENTS_CPP_UTILS_BLEADDRESS_H_
 #include "sdkconfig.h"
 #if defined(CONFIG_BT_ENABLED)
-#include <esp_gap_ble_api.h> // ESP32 BLE
-#include <string>
+#include <esp_gap_ble_api.h>  // ESP32 BLE
 
+#include <string>
 
 /**
  * @brief A %BLE device address.
@@ -19,15 +19,17 @@
  * Every %BLE device has a unique address which can be used to identify it and form connections.
  */
 class BLEAddress {
-public:
-	BLEAddress(esp_bd_addr_t address);
-	BLEAddress(std::string stringAddress);
-	bool           equals(BLEAddress otherAddress);
-	esp_bd_addr_t* getNative();
-	std::string    toString();
+   public:
+    BLEAddress(esp_bd_addr_t address, esp_ble_wl_addr_type_t type = BLE_WL_ADDR_TYPE_RANDOM);
+    BLEAddress(std::string stringAddress, esp_ble_wl_addr_type_t type = BLE_WL_ADDR_TYPE_RANDOM);
+    bool equals(BLEAddress otherAddress);
+    esp_bd_addr_t* getNative();
+    std::string toString();
+    esp_ble_wl_addr_type_t getType() const;
 
-private:
-	esp_bd_addr_t m_address;
+   private:
+    esp_bd_addr_t m_address;
+    esp_ble_wl_addr_type_t m_type;
 };
 
 #endif /* CONFIG_BT_ENABLED */
